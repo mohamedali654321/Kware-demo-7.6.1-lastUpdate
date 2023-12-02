96b75eb409571ad1584495b03e09d67f29b50fc8
root <root@ip-172-31-13-9.eu-west-1.compute.internal>
2023-12-02 14:27:15 +0300

    first commit

--- src/app/info/end-user-agreement/end-user-agreement-content/end-user-agreement-content.component.ts
+++ src/app/info/end-user-agreement/end-user-agreement-content/end-user-agreement-content.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { LocaleService } from 'src/app/core/locale/locale.service'; //kware-edit
 @Component({
   selector: 'ds-end-user-agreement-content',
   templateUrl: './end-user-agreement-content.component.html',
@@ -9,4 +9,10 @@ import { Component } from '@angular/core';
  * Component displaying the contents of the End User Agreement
  */
 export class EndUserAgreementContentComponent {
+//kware-edit
+  constructor(
+    public localeService: LocaleService ,/* kware edit - call service from LocaleService */
+  ){}
+  currentLocale: boolean = this.localeService.getCurrentLanguageCode() === 'ar' ? false : true;
+  //kware-edit end
 }
